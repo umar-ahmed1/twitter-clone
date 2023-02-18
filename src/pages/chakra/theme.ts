175b999897b1309ffa929c360bd00d88abbb9ece
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-02-18 01:25:12 -0700

    initialization

---
+++ src/pages/chakra/theme.ts
@@ -0,0 +1,30 @@
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react"
+import '@fontsource/open-sans/300.css'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/700.css'
+
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#1D9BF0",
+      // ...
+      900: "#E7E9EA",
+    },
+  },
+  fonts: {
+    body: 'Open Sans, sans-serif'
+  },
+  styles:{
+    global: () => ({
+        body: {
+            bg: 'black'
+        }
+    })
+  },
+  components: {
+
+  }
+})
+
